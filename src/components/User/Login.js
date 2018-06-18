e2c4a5debe9a0f11644d744cd646bcab55c668a6
John Datserakis <johndatserakis@gmail.com>
2018-06-17 20:09:45 -0400

    added login styles

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import { Button, Input, UserActionPanel } from 'theme/global'
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
     render() {
         return (
-            <div>Login</div>
+            <div className="container py-4">
+                <div className="row justify-content-center">
+                    <div className="col-lg-5">
+
+                        <UserActionPanel>
+                            <h1>Login</h1>
+
+                            <form onSubmit={this.handleSubmit}>
+                                <label>Username</label>
+                                <Input type="text" name="username" value={this.state.username} onChange={this.handleInputChange} />
+
+                                <label>Password</label>
+                                <Input type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
+
+                                <Button styleType="green" className="mt-2 w-100" type="submit" value="Submit">Login</Button>
+                            </form>
+                        </UserActionPanel>
+
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- src/theme/global.js
+++ src/theme/global.js
@@ -1,4 +1,5 @@
 import styled, { injectGlobal } from 'styled-components'
+import { darken } from 'polished';
 
 // Set main theme variables
 const themeMain = {
@@ -6,7 +7,8 @@ const themeMain = {
     black: '#333333',
     grey: '#eeeeee',
     blue: '#488ED8',
-    yellow: 'yellow'
+    yellow: 'yellow',
+    green: '#52C989'
 }
 
 // For gloabl css
@@ -33,13 +35,44 @@ injectGlobal`
     }
 `
 
+// Button
 const Button = styled.button`
     font-size: 1rem;
     border-radius: 4px;
     padding: 0.25rem 1rem;
-    margin: 0 1rem;
-    background: ${props => props.theme.blue};
+    margin: 0;
+    border: 1px solid ${props => props ? props.theme[props.styleType] : props.theme.blue};
+    background: ${props => props ? props.theme[props.styleType] : props.theme.blue};
     color: ${props => props.theme.white};
+
+
+
+    // &:hover {
+    //     background: ${props => darken(0.10, props.theme.blue)};
+    // }
 `
 
-export { themeMain, Button }
+const Input = styled.input`
+    display: block;
+    width: 100%;
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: ${props => props.theme.black};
+    background-color: ${props => props.theme.white};
+    background-clip: padding-box;
+    border: 1px solid ${props => props.theme.grey};
+    border-radius: 4px;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    margin-bottom: 15px;
+`;
+
+// User
+const UserActionPanel = styled.div`
+    background: ${props => props.theme.blue};
+    color: ${props => props.theme.white};
+    padding: 30px 20px;
+    border-radius: 4px;
+`;
+
+export { themeMain, Button, Input, UserActionPanel }
