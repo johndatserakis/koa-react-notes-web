40afd8589e8b99b11224838edc34aac6d7f53112
John Datserakis <johndatserakis@gmail.com>
2018-06-24 10:35:16 -0400

    testing browser import

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/app.css';
 import App from 'App';
-import { BrowserRouter } from 'react-router-dom'
+import { Router } from 'react-router-dom'
 import history from 'router/history'
 import { Provider } from 'react-redux'
 import store from 'store'
 
 ReactDOM.render((
     <Provider store={store}>
-        <BrowserRouter history={history}>
+        <Router history={history}>
             <App />
-        </BrowserRouter>
+        </Router>
     </Provider>
 ), document.getElementById('root'));
