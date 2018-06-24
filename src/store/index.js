d9f91862bbe0bb32a0b4ac6320b3206e9c10ffeb
John Datserakis <johndatserakis@gmail.com>
2018-06-24 10:30:52 -0400

    adjusting redux compose

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,7 @@ const allReducers = combineReducers({
 
 const allStoreEnhancers = compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const store = createStore(allReducers, {}, allStoreEnhancers)
