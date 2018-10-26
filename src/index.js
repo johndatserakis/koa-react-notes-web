5b4aff008955e7f39d744d090889a23ad093a177
John Datserakis <johndatserakis@gmail.com>
2018-10-26 16:21:07 -0400

    testing removal of history part of router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from 'App';
 import { HashRouter } from 'react-router-dom'
-import history from 'router/history'
+// import history from 'router/history'
 import { Provider } from 'react-redux'
 import store from 'store'
 
 ReactDOM.render((
     <Provider store={store}>
-        <HashRouter history={history}>
+        <HashRouter>
             <App />
         </HashRouter>
     </Provider>
