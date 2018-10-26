a13f74be27d6e323e897e883bdbd05e233c5ab52
John Datserakis <johndatserakis@gmail.com>
2018-10-26 16:34:39 -0400

    testing build

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from 'App';
 import { BrowserRouter } from 'react-router-dom'
+import history from 'router/history'
 import { Provider } from 'react-redux'
 import store from 'store'
 
