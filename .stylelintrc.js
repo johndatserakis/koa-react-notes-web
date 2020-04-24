aa3a64c90f53ec000b74716c58d4069a07757146
John Datserakis <johndatserakis@gmail.com>
2020-04-23 22:45:45 -0400

    stylelint updates

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,24 @@
 module.exports = {
-  "plugins": [
-    "stylelint-order",
-    "stylelint-scss"
-  ],
+  "extends": "stylelint-config-recommended-scss",
+  "plugins": ["stylelint-order", "stylelint-scss", "stylelint-declaration-strict-value"],
   "rules": {
+    "color-hex-case": "lower",
+    "color-hex-length": "short",
     "color-no-invalid-hex": true,
-    "order/properties-alphabetical-order": true
+    "declaration-no-important": true,
+    "font-weight-notation": "numeric",
+    "function-url-quotes": "always",
+    "indentation": 2,
+    "no-duplicate-selectors": true,
+    "number-leading-zero": "always",
+    "order/order": ["custom-properties", "declarations"],
+    "order/properties-alphabetical-order": true,
+    "scale-unlimited/declaration-strict-value": [["/color/"], {
+      "ignoreKeywords": ["currentColor", "transparent", "inherit"]
+    }],
+    "selector-max-id": 0,
+    "string-quotes": "double",
+    "declaration-colon-space-after": "always",
+    "declaration-colon-space-before": "never",
   }
 }
